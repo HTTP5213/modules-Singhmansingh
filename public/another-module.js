b49b4714cbbe2fe37eeb1f78e9133ab2438161b3
Shavin Singh <64917885+Singhmansingh@users.noreply.github.com>
2023-03-29 09:03:25 -0400

    Added my module and another module

--- public/another-module.js
+++ public/another-module.js
@@ -1,1 +1,17 @@
+var HobbitSettlement = class {
+	constructor(name, founding, founders) {
+		this.name = name;
+		this.founding = founding;
+		this.founders = founders;
+		this.originalSettlement = { name: 'Eriador', details: eriadorDetails };
+	}
+};
 
+var theShire = new HobbitSettlement('The Shire', 1601, ['Marcho', 'Blanco']);
+var buckland = new HobbitSettlement('Buckland', 2340, 'Gorhendad Oldbuck');
+
+module.exports = {
+    HobbitSettlement,
+    theShire,
+    buckland
+}
--- public/my-module.js
+++ public/my-module.js
@@ -1,1 +1,8 @@
+import { theShire, buckland } from './another-module.js';
 
+var statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
+
+module.exports = {
+    statement,
+    theShire
+}
